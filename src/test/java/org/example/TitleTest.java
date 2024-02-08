3198568d0604900539e03f2d3a9879302d2aae59
Dariusz Hryciuk <dhryciuk@sii.pl>
2024-02-08 19:38:07 +0100

    ok

--- src/test/java/org/example/TitleTest.java
+++ src/test/java/org/example/TitleTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class TitleTest extends BaseTest {
 
     @Test
+    @Tag("regresja")
     @DisplayName("Testing title")
     void shouldValidateCorrectTitle() {
         String actualTitle = driver.getTitle();
